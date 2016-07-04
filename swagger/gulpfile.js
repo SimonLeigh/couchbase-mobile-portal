18383b494be53dc5b80a40c65f143b274eb2b98c
James Nocentini <james.nocentini@gmail.com>
2016-07-04 13:18:02 +0100

    Refactor some endpoints to be in their own file and update gulpfile

--- swagger/gulpfile.js
+++ swagger/gulpfile.js
@@ -1,10 +1,31 @@
 var gulp = require('gulp');
 var swagger = require('gulp-swagger');
 
+gulp.task('public', function () {
+  gulp.src('./public.yaml')
+    .pipe(swagger('full-public.json'))
+    .pipe(gulp.dest('./tmp'))
+});
+
 gulp.task('admin', function () {
   gulp.src('./admin.yaml')
     .pipe(swagger('full-admin.json'))
     .pipe(gulp.dest('./tmp'))
 });
 
-gulp.task('default', ['admin']);
+gulp.task('lite', function () {
+  gulp.src('./lite.yaml')
+    .pipe(swagger('full-lite.json'))
+    // run the swagger js code
+    .pipe(gulp.dest('./tmp'))
+});
+
+gulp.task('custom', function () {
+  console.log('Gulp is running!!');
+});
+
+gulp.task('default', ['public', 'admin', 'lite', 'watch']);
+
+gulp.task('watch', function () {
+  gulp.watch('**/*.yaml', ['public', 'admin', 'lite']);
+});
