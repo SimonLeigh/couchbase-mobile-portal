96c6b0ed385dbaf36b2a6379ee73e039a5f6d065
James Nocentini <james.nocentini@gmail.com>
2016-07-25 17:21:53 +0100

    Fix #410

--- swagger/gulpfile.js
+++ swagger/gulpfile.js
@@ -1,27 +1,65 @@
-var gulp = require('gulp');
-var swagger = require('gulp-swagger');
+var gulp = require('gulp')
+  , concat = require('gulp-concat')
+  , runSequence = require('run-sequence')
+  , swagger = require('gulp-swagger');
 
-gulp.task('public', function () {
-  gulp.src('./public.yaml')
+/**
+ * Concat the parameters into parameters/index.yaml file
+ */
+gulp.task('params-cbl', function () {
+  return gulp.src(['parameters/common.yaml', 'parameters/cbl.yaml'])
+    .pipe(concat('index.yaml'))
+    .pipe(gulp.dest('parameters'));
+});
+
+gulp.task('params-sg', function () {
+  return gulp.src(['parameters/common.yaml', 'parameters/sg.yaml'])
+    .pipe(concat('index.yaml'))
+    .pipe(gulp.dest('parameters'));
+});
+
+/**
+ * Concat the definitions into definitions/index.yaml file
+ */
+gulp.task('definitions-cbl', function () {
+  return gulp.src(['definitions/common.yaml', 'definitions/cbl.yaml'])
+    .pipe(concat('index.yaml'))
+    .pipe(gulp.dest('definitions'));
+});
+
+gulp.task('definitions-sg', function () {
+  return gulp.src(['definitions/common.yaml', 'definitions/sg.yaml'])
+    .pipe(concat('index.yaml'))
+    .pipe(gulp.dest('definitions'));
+});
+
+
+gulp.task('public', ['params-sg', 'definitions-sg'], function () {
+  return gulp.src('./public.yaml')
     .pipe(swagger('public.json'))
     .pipe(gulp.dest('./tmp'))
 });
 
-gulp.task('admin', function () {
-  gulp.src('./admin.yaml')
+gulp.task('admin', ['params-sg', 'definitions-sg'], function () {
+  return gulp.src('./admin.yaml')
     .pipe(swagger('admin.json'))
     .pipe(gulp.dest('./tmp'))
 });
 
-gulp.task('lite', function () {
-  gulp.src('./lite.yaml')
+gulp.task('lite', ['params-cbl', 'definitions-cbl'], function () {
+  return gulp.src('./lite.yaml')
     .pipe(swagger('lite.json'))
     // run the swagger js code
     .pipe(gulp.dest('./tmp'))
 });
 
-gulp.task('build', ['public', 'admin', 'lite']);
+gulp.task('build', function(done) {
+  runSequence('public', 'admin', 'lite', function() {
+    console.log('Run something else');
+    done();
+  });
+});
 
-gulp.task('watch', ['public', 'admin', 'lite'], function () {
-  gulp.watch('**/*.yaml', ['public', 'admin', 'lite']);
+gulp.task('watch', ['build'], function () {
+  gulp.watch('**/*.yaml', ['build']);
 });
